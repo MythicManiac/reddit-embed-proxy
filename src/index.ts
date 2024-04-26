c57d6320a7a1c1b445579151dab0cfe7909575ba
Mythic <mythicmaniac@gmail.com>
2024-04-26 14:17:52 +0300

    Simple reddit proxy

--- src/index.ts
+++ src/index.ts
@@ -27,6 +27,6 @@ export interface Env {
 
 export default {
   async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-    return new Response("Hello World!");
+    return await fetch("https://reddit.com", request);
   },
 };
